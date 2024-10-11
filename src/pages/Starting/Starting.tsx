9760f66980e83c0af5940f8b553f71019fef17d0
Swejan17 <swejanchowdary01072004@gmail.com>
2024-10-11 20:58:27 +0530

    done with

--- src/pages/Starting/Starting.tsx
+++ src/pages/Starting/Starting.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 const Starting = () => {
   return (
   
-    <div className="dashboard" >
+    <div className="dashboard " style={{width:"650px"}} >
           
-          <img src="/Mind_Your_Finance_Clear_Background.png" style={{width:"300px"  ,position:"absolute",left:"20px", top:"250px" } } onClick={() => window.location.href = '/landing'} />
+          <img src="/Mind_Your_Finance_Clear_Background.png" style={{width:"300px"  ,position:"absolute",left:"40px", top:"250px" } } onClick={() => window.location.href = '/landing'} />
     </div>
   )
 }
